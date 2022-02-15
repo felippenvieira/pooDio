7e62fe62d8453192bf321efd0c3d97632b36f725
Felippe <felippe_nvieira@hotmail.com>
2022-02-15 17:52:31 -0300

    Programming using OOP.

--- pooDio/Program.cs
+++ pooDio/Program.cs
@@ -1,10 +1,4 @@
-﻿string heroName = "Arus";
-int heroLevel = 42;
-string heroClass = "Knight";
+﻿using pooDio.src.Entities;
 
-string heroName2 = "Wedge";
-int heroLevel2 = 37;
-string heroClass2 = "Ninja";
-
-Console.WriteLine(heroName + " " + heroLevel + " " + heroClass);
+Arus hero = new Arus();
 
---
+++ pooDio/src/Entities/Arus.cs
@@ -0,0 +1,9 @@
+﻿namespace pooDio.src.Entities
+{
+    internal class Arus
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Type { get; set; }
+    }
+}
