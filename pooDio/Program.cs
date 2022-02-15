29eb0310a4bdf682934140becf1907da91288c05
Felippe <felippe_nvieira@hotmail.com>
2022-02-15 17:42:31 -0300

    Programming without OOP

--- pooDio/Program.cs
+++ pooDio/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string heroName = "Arus";
+int heroLevel = 42;
+string heroClass = "Knight";
+
+string heroName2 = "Wedge";
+int heroLevel2 = 37;
+string heroClass2 = "Ninja";
+
+Console.WriteLine(heroName + " " + heroLevel + " " + heroClass);
+
