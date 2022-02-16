1a3629905324b4b696c5e03853bcad7a8237fb3f
Felippe <felippe_nvieira@hotmail.com>
2022-02-16 15:55:50 -0300

    Updated black wizard class

--- pooDio/src/Entities/BlackWizard.cs
+++ pooDio/src/Entities/BlackWizard.cs
@@ -13,23 +13,5 @@ namespace pooDio.src.Entities
             Health = 70;
             Mana = 250;
         }
-
-        public override string Attack(string attack)
-        {
-            return Name + " atacked with his " + attack + ".";
-        }
-
-        public override string Attack(string attack, int bonus)
-        {
-            if (bonus > 10)
-            {
-                return Name + " atacked with his super effective " + attack + " and dealt " + bonus + " bonus damage.";
-            }
-            else
-            {
-                return Name + " atacked with his weak " + attack + " and dealt " + bonus + " bonus damage.";
-            }
-        }
-
     }
 }
